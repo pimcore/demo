a3232498591c2c685d4659bd768e589d0f1bd49d
Divesh <divesh.pahuja@pimcore.com>
2019-07-17 19:05:38 +0200

    [Documents][Areabrick] product teaser

---
+++ src/AppBundle/Document/Areabrick/ProductTeaser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Document\Areabrick;
+
+class ProductTeaser extends AbstractAreabrick
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'Product Teaser';
+    }
+}
