a9a23103aa647296f6a2cb878ecaf98cc4ead155
Divesh <divesh.pahuja@pimcore.com>
2019-07-17 19:04:14 +0200

    [Documents][Areabrick] hero grid #101

---
+++ src/AppBundle/Document/Areabrick/HeroGrid.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Document\Areabrick;
+
+class HeroGrid extends AbstractAreabrick
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'Hero Grid';
+    }
+}
