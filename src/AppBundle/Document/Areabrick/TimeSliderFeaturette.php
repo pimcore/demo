b18feae0817709eef16166ccd75e7c6e7a26b5ad
Divesh <divesh.pahuja@pimcore.com>
2019-07-22 13:16:23 +0200

    [Documents][Areabrick] time-slider featurette #17

---
+++ src/AppBundle/Document/Areabrick/TimeSliderFeaturette.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Document\Areabrick;
+
+class TimeSliderFeaturette extends AbstractAreabrick
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'Time Slider Featurette';
+    }
+}
