faa218889715f5d738a3fa48f598972d460c76a7
Divesh <divesh.pahuja@pimcore.com>
2019-08-23 17:34:31 +0200

    [Navigation] Fix icons and subpages in mobile view #109 #112

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -14,9 +14,26 @@ $(".navbar ul.navbar-nav>li>ul").each(function () {
 
 });
 
-$("ul.dropdown-submenu").each(function () {
-    $(this).parent('li').addClass('submenu-icon');
-});
+if (window.matchMedia('(max-width: 600px)').matches)
+{
+    $("ul.dropdown-menu").each(function () {
+        $(this).parent('li').addClass('main-menu-icon');
+    });
+
+    $('li.main-menu-icon>a').on('touch touchstart', function(event){
+        event.preventDefault();
+
+        $(this).parent('li').toggleClass('active-icon');
+        $(this).next('ul').toggle(
+            function(){$(this).attr({"display": "block"});},
+            function(){$(this).attr({"display": "none"});}
+        );
+    });
+} else {
+    $("ul.dropdown-submenu").each(function () {
+        $(this).parent('li').addClass('submenu-icon');
+    });
+}
 
 $('.search-button').on('click', '.search-toggle', function(e) {
     var selector = $(this).data('selector');
