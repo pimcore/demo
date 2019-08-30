1ca1d018f2b124c3692c323a50e5e000ef686d0c
Christian Fasching <christian.fasching@elements.at>
2019-08-30 16:35:39 +0200

    added missing files

---
+++ src/AppBundle/Document/Areabrick/HeroTeaser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Document\Areabrick;
+
+class HeroTeaser extends AbstractAreabrick
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'Hero Teaser';
+    }
+}
