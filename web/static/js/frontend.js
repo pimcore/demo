b7e88203883674adc2027bccc97be953bec67a50
Divesh <divesh.pahuja@pimcore.com>
2019-08-30 10:30:44 +0200

    return to top for mobile view

--- web/static/js/frontend.js
+++ web/static/js/frontend.js
@@ -13,4 +13,18 @@ $(document).ready(function () {
 
     $(".image-hotspot").tooltip();
     $(".image-marker").tooltip();
+
+    //scroll to top
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#back-to-top').addClass('display-block');
+        } else {
+            $('#back-to-top').removeClass('display-block');
+        }
+    });
+
+    $('#back-to-top').click(function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        return false;
+    });
 });
