e350b6efa8e8d2a3c9351f4717ddc5795bdb8e52
Divesh <divesh.pahuja@pimcore.com>
2019-09-12 16:45:20 +0200

    events page

---
+++ web/static/js/vue-app.js
@@ -0,0 +1,180 @@
+const apolloClient = new Apollo.lib.ApolloClient({
+  networkInterface: Apollo.lib.createNetworkInterface({
+    uri: '/pimcore-graphql-webservices/events?apikey=8e0935fe948ccf727342e86015c00834',
+    transportBatching: true,
+  }),
+  connectToDevTools: true,
+});
+
+const apolloProvider = new VueApollo.ApolloProvider({
+  defaultClient: apolloClient,
+});
+
+const GET_EVENTS = Apollo.gql `
+query getEventListing($queryFilter: String) {
+  events: getEventListing(sortBy: "fromDate",sortOrder: "ASC", filter: $queryFilter) {
+    list: edges {
+      details: node {
+        id
+        title
+        locationAddress
+        fromDate
+        toDate
+        images {
+          image {
+            fullpath
+          }
+        }
+        tags
+      }
+    }
+  }
+}
+`;
+
+const GET_EVENT_DETAILS = Apollo.gql `
+query getEventDetails($id: Int!) {
+  eventDetails: getEvent(id: $id) {
+      id
+      title
+      description
+      locationAddress
+      fromDate
+      toDate
+      images {
+        image {
+          fullpath
+        }
+      }
+      video {
+          type
+          data {
+            ... on VideoDataDescriptor {
+              id
+            }
+          }
+      }
+      contactName
+      contactPhone
+      contactEmail
+      contactAddress
+    }
+}
+`;
+
+new Vue({
+  el: '#vueapp',
+  delimiters: ['[[',']]'],
+  data() {
+    return {
+      eventList: null,
+      eventDetails: null,
+      loading: false,
+      errored: false,
+      activePanel: 1,
+      filters: {
+        fromDate: null,
+        toDate: null,
+        tag: 'All'
+      }
+    }
+  },
+  components: {
+    vuejsDatepicker
+  },
+  apolloProvider,
+  apollo: {
+    eventList: {
+      query: GET_EVENTS,
+      update: eventList => eventList.events.list,
+      error(error) { // Catch the error
+        console.log(error);
+        this.errored = true;
+      }
+    }
+  },
+  filters: {
+    dateFormat: function(value) {
+      if (value) {
+        return new Date(value).toDateString();
+      }
+    },
+    timeFormat: function(value) {
+      if (value) {
+        return new Date(value).toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric"
+        });
+      }
+    }
+  },
+  methods: {
+    filterListing: function() {
+      filters = new Object();
+
+      if (this.filters.fromDate) {
+        var fromTimeStamp = +new Date(this.filters.fromDate.setHours(0, 0, 0, 0) / 1000);
+        filters.fromDate = {
+          $gte: fromTimeStamp
+        };
+      }
+      if (this.filters.toDate) {
+        var toTimeStamp = +new Date(this.filters.toDate.setHours(23, 59, 59, 999) / 1000);
+        filters.toDate = {
+          $lte: toTimeStamp
+        };
+      }
+      if(!$.isEmptyObject(filters)) {
+        filters = JSON.stringify(filters);
+      } else filters = null
+
+      this.$apollo.queries.eventList.refetch({
+        queryFilter: filters
+      });
+    },
+    clearFilters: function() {
+      this.filters.fromDate = null;
+      this.filters.toDate =null;
+      this.filters.tag = 'All';
+      this.filterListing();
+    },
+    openDetailPanel: function(id) {
+      this.$apollo.query({
+        query: GET_EVENT_DETAILS,
+        variables: {
+          id: id,
+        },
+      }).then(data => {
+        this.eventDetails = data.data.eventDetails;
+        this.activePanel = 2;
+        this.scrollToTop();
+      });
+
+    },
+    changePanel(panelId) {
+      this.activePanel = panelId;
+      this.scrollToTop();
+    },
+    scrollToTop: function() {
+      window.scrollTo(0, 0);
+    },
+    compareDates(fromDate, toDate) {
+      return new Date(toDate).setHours(0, 0, 0, 0) > new Date(fromDate).setHours(0, 0, 0, 0);
+    }
+  },
+  computed: {
+    filterResult: function() {
+      var vm = this;
+      var tag = vm.filters.tag;
+
+      if (tag === "All") {
+        return vm.eventList;
+      } else {
+        return vm.eventList.filter(function(event) {
+          return event.details.tags.indexOf(tag) >= 0;
+
+        });
+      }
+    }
+  }
+});
