ea4b680631d34a00bf99aee0ede46e6f25fd8c45
Bernhard Rusch <bernhard.rusch@elements.at>
2019-09-12 17:03:50 +0200

    added matomo config

---
+++ var/config/reports.php
@@ -0,0 +1,51 @@
+<?php 
+
+return [
+    "analytics" => [
+        "sites" => [
+            "default" => [
+                "profile" => "",
+                "trackid" => "admin",
+                "gtagcode" => FALSE,
+                "asynchronouscode" => FALSE,
+                "retargetingcode" => FALSE,
+                "additionalcode" => "",
+                "additionalcodebeforepageview" => "",
+                "additionalcodebeforeinit" => "",
+                "accountid" => "",
+                "internalid" => "",
+                "universal_configuration" => ""
+            ]
+        ]
+    ],
+    "webmastertools" => [
+        "sites" => [
+            "default" => [
+                "verification" => ""
+            ]
+        ]
+    ],
+    "tagmanager" => [
+        "sites" => [
+            "default" => [
+                "containerId" => ""
+            ]
+        ]
+    ],
+    "piwik" => [
+        "piwik_url" => "https://matomo.pimcore.fun/",
+        "api_token" => "9823a620d1bffd66f911c4ade504a65b",
+        "report_token" => "9823a620d1bffd66f911c4ade504a65b",
+        "api_client_options" => "",
+        "iframe_username" => "api",
+        "iframe_password" => "80aa807e9bfa23459348c63e842beb70",
+        "sites" => [
+            "default" => [
+                "site_id" => 2,
+                "code_before_init" => "",
+                "code_before_track" => "",
+                "code_after_track" => ""
+            ]
+        ]
+    ]
+];
