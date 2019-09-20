2355093f4c862988583688cc34a1acd03ff98a25
Christian Fasching <christian.fasching@elements.at>
2019-09-20 15:57:57 +0200

    updated web2print settings

--- var/config/web2print.php
+++ var/config/web2print.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
 
 return [
     "enableInDefaultView" => TRUE,
-    "displayfield-1243-inputEl" => "Enables Web2Print documents in default documents view in default perspective. Either activate this or create custom views and perspectives for managing Web2Print documents.",
+    "displayfield-1581-inputEl" => "Enables Web2Print documents in default documents view in default perspective. Either activate this or create custom views and perspectives for managing Web2Print documents.",
     "generalTool" => "pdfreactor",
     "generalDocumentSaveMode" => "default",
-    "displayfield-1246-inputEl" => "Document Save Mode = cleanup deletes all not used document elements for current document. This might be necessary for useage reports in print documents.",
+    "displayfield-1584-inputEl" => "Document Save Mode = cleanup deletes all not used document elements for current document. This might be necessary for useage reports in print documents.",
     "pdfreactorVersion" => "10.0",
-    "pdfreactorProtocol" => "http",
-    "pdfreactorServer" => "",
+    "pdfreactorProtocol" => "https",
+    "pdfreactorServer" => "cloud.pdfreactor.com",
     "pdfreactorServerPort" => "9410",
-    "pdfreactorBaseUrl" => "",
-    "displayfield-1230-inputEl" => "BaseURL for PDFreactor. This is prefixed to each relative url in print templates when creating PDFs.",
+    "pdfreactorBaseUrl" => "https://demo.pimcore.fun",
+    "displayfield-1568-inputEl" => "BaseURL for PDFreactor. This is prefixed to each relative url in print templates when creating PDFs.",
     "pdfreactorApiKey" => "",
-    "displayfield-1232-inputEl" => "If the PDFreactor instance is set up to require API keys, you can enter it here.",
+    "displayfield-1570-inputEl" => "If the PDFreactor instance is set up to require API keys, you can enter it here.",
     "pdfreactorLicence" => "",
     "pdfreactorEnableDebugMode" => FALSE,
     "wkhtmltopdfBin" => "wkhtmltopdf",
@@ -21,6 +21,6 @@ return [
         "print-media-type" => "",
         "page-size" => "A4"
     ],
-    "displayfield-1238-inputEl" => "One per line with '--' and whitespace between key and value (e.g. --key value)",
+    "displayfield-1576-inputEl" => "One per line with '--' and whitespace between key and value (e.g. --key value)",
     "wkhtml2pdfHostname" => "http://demo.pimcore.org"
 ];
