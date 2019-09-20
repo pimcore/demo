33c856b4348411fb9ca0763a07c616bc635bf718
Divesh <divesh.pahuja@pimcore.com>
2019-09-20 11:22:52 +0200

    Events page - mobile UI, resolves #138, resolves #139, resolves #140

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,40 +1,40 @@
-// main menu
-$(".navbar ul.navbar-nav>li>ul").each(function () {
-    var li = $(this).parent();
-    var a = $("a.nav-link", li);
-
-    $(this).addClass("dropdown-menu");
-    li.addClass("dropdown");
-    li.on("mouseenter", function () {
-        $("ul.dropdown", $(this)).show();
-    });
-    li.on("mouseleave", function () {
-        $("ul.dropdown", $(this)).hide();
-    });
-
-});
-
 if (window.matchMedia('(max-width: 600px)').matches)
 {
+    //mobile main navigation
     $("ul.dropdown-menu").each(function () {
         $(this).parent('li').addClass('main-menu-icon');
     });
 
-    $('li.main-menu-icon>a').on('touch touchstart', function(event){
-        event.preventDefault();
-
-        $(this).parent('li').toggleClass('active-icon');
-        $(this).next('ul').toggle(
-            function(){$(this).attr({"display": "block"});},
-            function(){$(this).attr({"display": "none"});}
-        );
+    $('li.main-menu-icon>a').on('click', function (e) {
+        if (e.offsetX > this.offsetWidth - 100) {
+            e.preventDefault();
+            $(this).parent('li').toggleClass('active-icon');
+            $(this).next('ul').toggle();
+        }
     });
 } else {
+    //  desktop main navigation
+    $(".navbar ul.navbar-nav>li>ul").each(function () {
+        var li = $(this).parent();
+        var a = $("a.nav-link", li);
+
+        $(this).addClass("dropdown-menu");
+        li.addClass("dropdown");
+        li.on("mouseenter", function () {
+            $("ul.dropdown", $(this)).show();
+        });
+        li.on("mouseleave", function () {
+            $("ul.dropdown", $(this)).hide();
+        });
+
+    });
+
     $("ul.dropdown-submenu").each(function () {
         $(this).parent('li').addClass('submenu-icon');
     });
 }
 
+//search bar
 $('.search-button').on('click', '.search-toggle', function(e) {
     var selector = $(this).data('selector');
 
