33e6aecf31f49c5fe9a6c4f57557abd109560ba5
brusch <bernhard.rusch@elements.at>
2020-11-11 14:42:51 +0100

    use new controller notation

--- dump/data-1-documents_snippet.sql
+++ dump/data-1-documents_snippet.sql
@@ -3,13 +3,13 @@ SET NAMES utf8mb4;
 
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('33',NULL,'default','default','snippets/standard-teaser.html.twig',NULL,NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('35',NULL,'default','default','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('36','AppBundle','Default','default','includes/sidebar.html.twig',NULL,NULL);
+INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('36',NULL,'Default','default','includes/sidebar.html.twig',NULL,NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('106',NULL,'default','default','snippets/standard-teaser.html.twig',NULL,NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('107',NULL,'default','default','snippets/standard-teaser.html.twig',NULL,NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('114',NULL,'default','default','includes/sidebar.html.twig',NULL,NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('116',NULL,'default','default','snippets/standard-teaser.html.twig',NULL,NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('117',NULL,'default','default','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('150','AppBundle','Default','default','includes/sidebar.html.twig','0',NULL);
+INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('150',NULL,'Default','default','includes/sidebar.html.twig','0',NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('166',NULL,'default','default','snippets/standard-teaser.html.twig','33',NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('167',NULL,'default','default','snippets/standard-teaser.html.twig','35',NULL);
 INSERT INTO documents_snippet (`id`,`module`,`controller`,`action`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES ('168',NULL,'default','default','snippets/standard-teaser.html.twig','106',NULL);
