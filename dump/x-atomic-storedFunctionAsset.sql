9cd2f0213854473ff453195e049ab8a700df4544
Matthias Baur <m.baur@syseleven.de>
2021-01-28 16:55:45 +0100

    Add MySQL compatibility for function
    This copies functions from pimcore/customer-data-framework.
    
    Especially the fixes from
    https://github.com/pimcore/customer-data-framework/pull/112 seem to be
    relevant.

--- dump/x-atomic-storedFunctionAsset.sql
+++ dump/x-atomic-storedFunctionAsset.sql
@@ -1,18 +1,24 @@
 DROP FUNCTION IF EXISTS PLUGIN_CMF_COLLECT_ASSET_SEGMENT_ASSIGNMENTS;
+DELIMITER //
 
 CREATE FUNCTION PLUGIN_CMF_COLLECT_ASSET_SEGMENT_ASSIGNMENTS(elementIdent INT) RETURNS TEXT
+  READS SQL DATA
   BEGIN
     DECLARE segmentIds TEXT;
+    DECLARE elementExists TINYINT;
     DECLARE breaks TINYINT;
 
     SELECT `segments` FROM `plugin_cmf_segment_assignment` WHERE `elementId` = elementIdent AND `elementType` = 'asset' INTO segmentIds;
+    SELECT COUNT(*) FROM `plugin_cmf_segment_assignment` WHERE `elementId` = elementIdent AND `elementType` = 'asset' INTO elementExists;
     SELECT `breaksInheritance` FROM `plugin_cmf_segment_assignment` WHERE `elementId` = elementIdent AND `elementType` = 'asset' INTO breaks;
 
-    WHILE breaks <> 1 AND elementIdent > 1 DO
+    WHILE (elementExists = 0 OR breaks IS NULL OR breaks <> 1) AND elementIdent > 1 DO
       SELECT `parentId`  FROM `assets` WHERE `id` = elementIdent INTO elementIdent;
       SELECT CONCAT_WS(',', segmentIds, (SELECT `segments` FROM `plugin_cmf_segment_assignment` WHERE `elementId` = elementIdent AND `elementType` = 'asset')) INTO segmentIds;
+      SELECT COUNT(*) FROM `plugin_cmf_segment_assignment` WHERE `elementId` = elementIdent AND `elementType` = 'asset' INTO elementExists;
       SELECT `breaksInheritance` INTO breaks FROM `plugin_cmf_segment_assignment` WHERE `elementId` = elementIdent AND `elementType` = 'asset';
     END WHILE;
 
     RETURN segmentIds;
-  END
+  END//
+DELIMITER ;
