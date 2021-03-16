6db2cc17fa02ff2b2605349e248c54beb1339dbb
Bernhard Rusch <bernhard.rusch@elements.at>
2021-03-16 18:32:17 +0100

    fixed editable dialog

--- src/Document/Areabrick/Blockquote.php
+++ src/Document/Areabrick/Blockquote.php
@@ -15,8 +15,38 @@
 
 namespace App\Document\Areabrick;
 
-class Blockquote extends AbstractAreabrick
+use Pimcore\Extension\Document\Areabrick\EditableDialogBoxConfiguration;
+use Pimcore\Extension\Document\Areabrick\EditableDialogBoxInterface;
+use Pimcore\Model\Document\Editable;
+
+class Blockquote extends AbstractAreabrick implements EditableDialogBoxInterface
 {
+    public function getEditableDialogBoxConfiguration(Editable $area, ?Editable\Area\Info $info): EditableDialogBoxConfiguration
+    {
+        $config = new EditableDialogBoxConfiguration();
+        $config->setWidth(600);
+        //$config->setReloadOnClose(true);
+        $config->setItems([
+            [
+                'type' => 'input',
+                'label' => 'Some additional Text', // labels are optional
+                'name' => 'myDialogInput'
+            ],
+            [
+                'type' => 'checkbox',
+                'name' => 'myDialogCheckbox',
+                'label' => 'This is the checkbox label',
+            ],
+            [
+                'type' => 'date',
+                'name' => 'myDialogDate'
+            ]
+        ]);
+
+
+        return $config;
+    }
+
     public function needsReload(): bool
     {
         return false;
