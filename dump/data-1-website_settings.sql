6638a5f9fba4f53dd63d1edc2b519c3876ea697a
Bernhard Rusch <bernhard.rusch@elements.at>
2021-03-23 11:21:43 +0100

    Website settings are now stored in the database

---
+++ dump/data-1-website_settings.sql
@@ -0,0 +1,6 @@
+
+SET NAMES utf8mb4;
+
+INSERT INTO `website_settings` (`id`, `name`, `type`, `data`, `language`, `siteId`, `creationDate`, `modificationDate`) VALUES
+(1,	'fallbackFilterdefinition',	'object',	'563',	'',	0,	0,	1563893384),
+(2,	'defaultTaxClass',	'object',	'693',	'',	0,	0,	1565267369);
