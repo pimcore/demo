f804b59ee4a3ac45c1d0c97d171ac3375c2fe5ca
Christian Fasching <christian.fasching@pimcore.com>
2021-04-02 16:44:58 +0200

    updated dump

---
+++ dump/data-1-settings_store.sql
@@ -0,0 +1,4 @@
+
+SET NAMES utf8mb4;
+
+INSERT INTO settings_store (`id`,`scope`,`data`,`type`) VALUES ('BUNDLE_INSTALLED__CustomerManagementFrameworkBundle\\PimcoreCustomerManagementFrameworkBundle','pimcore','1','bool');
