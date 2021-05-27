d1cc83f0e32877a79f47696df13aa5bc7848ea6c
Divesh <divesh.pahuja@pimcore.com>
2021-05-27 14:57:21 +0200

    Avoid Pimcore internal code usage - resolves #227

--- src/Twig/Extension/LanguageSwitcherExtension.php
+++ src/Twig/Extension/LanguageSwitcherExtension.php
@@ -68,7 +68,7 @@ class LanguageSwitcherExtension extends AbstractExtension
     {
         $flag = '';
         if (Tool::isValidLanguage($language)) {
-            $flag = Tool::getLanguageFlagFile($language);
+            $flag = self::getLanguageFlagFile($language);
         }
         $flag = preg_replace('@^' . preg_quote(PIMCORE_WEB_ROOT, '@') . '@', '', $flag);
 
@@ -84,5 +84,42 @@ class LanguageSwitcherExtension extends AbstractExtension
             new TwigFunction('get_localized_links', [$this, 'getLocalizedLinks']),
             new TwigFunction('get_language_flag', [$this, 'getLanguageFlag'])
         ];
+    }
+
+    /**
+     * @param string $language
+     *
+     * @return string
+     */
+    public static function getLanguageFlagFile($language)
+    {
+        $basePath = '/bundles/pimcoreadmin/img/flags';
+        $code = strtolower($language);
+        $iconPath = $basePath . '/countries/_unknown.svg';
+
+        $languageCountryMapping = [
+            'aa' => 'er', 'af' => 'za', 'am' => 'et', 'as' => 'in', 'ast' => 'es', 'asa' => 'tz',
+            'az' => 'az', 'bas' => 'cm', 'eu' => 'es', 'be' => 'by', 'bem' => 'zm', 'bez' => 'tz', 'bg' => 'bg',
+            'bm' => 'ml', 'bn' => 'bd', 'br' => 'fr', 'brx' => 'in', 'bs' => 'ba', 'cs' => 'cz', 'da' => 'dk',
+            'de' => 'de', 'dz' => 'bt', 'el' => 'gr', 'en' => 'gb', 'es' => 'es', 'et' => 'ee', 'fi' => 'fi',
+            'fo' => 'fo', 'fr' => 'fr', 'ga' => 'ie', 'gv' => 'im', 'he' => 'il', 'hi' => 'in', 'hr' => 'hr',
+            'hu' => 'hu', 'hy' => 'am', 'id' => 'id', 'ig' => 'ng', 'is' => 'is', 'it' => 'it', 'ja' => 'jp',
+            'ka' => 'ge', 'os' => 'ge', 'kea' => 'cv', 'kk' => 'kz', 'kl' => 'gl', 'km' => 'kh', 'ko' => 'kr',
+            'lg' => 'ug', 'lo' => 'la', 'lt' => 'lt', 'mg' => 'mg', 'mk' => 'mk', 'mn' => 'mn', 'ms' => 'my',
+            'mt' => 'mt', 'my' => 'mm', 'nb' => 'no', 'ne' => 'np', 'nl' => 'nl', 'nn' => 'no', 'pl' => 'pl',
+            'pt' => 'pt', 'ro' => 'ro', 'ru' => 'ru', 'sg' => 'cf', 'sk' => 'sk', 'sl' => 'si', 'sq' => 'al',
+            'sr' => 'rs', 'sv' => 'se', 'swc' => 'cd', 'th' => 'th', 'to' => 'to', 'tr' => 'tr', 'tzm' => 'ma',
+            'uk' => 'ua', 'uz' => 'uz', 'vi' => 'vn', 'zh' => 'cn', 'gd' => 'gb-sct', 'gd-gb' => 'gb-sct',
+            'cy' => 'gb-wls', 'cy-gb' => 'gb-wls', 'fy' => 'nl', 'xh' => 'za', 'yo' => 'bj', 'zu' => 'za',
+            'ta' => 'lk', 'te' => 'in', 'ss' => 'za', 'sw' => 'ke', 'so' => 'so', 'si' => 'lk', 'ii' => 'cn',
+            'zh-hans' => 'cn', 'sn' => 'zw', 'rm' => 'ch', 'pa' => 'in', 'fa' => 'ir', 'lv' => 'lv', 'gl' => 'es',
+            'fil' => 'ph',
+        ];
+
+        if (array_key_exists($code, $languageCountryMapping)) {
+            $iconPath = $basePath . '/countries/' . $languageCountryMapping[$code] . '.svg';
+        }
+
+        return $iconPath;
     }
 }
