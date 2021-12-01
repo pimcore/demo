0d32ff1f81d56e7b2771d260e81167be1c8038e9
dvesh3 <divesh.pahuja@pimcore.com>
2021-12-01 17:18:15 +0100

    [Dump] Add entry to settingsstore table to mark Datahub bundle installed

--- dump/data-1-settings_store.sql
+++ dump/data-1-settings_store.sql
@@ -3,3 +3,5 @@ SET NAMES utf8mb4;
 
 INSERT INTO settings_store (`id`,`scope`,`data`,`type`) VALUES ('BUNDLE_INSTALLED__CustomerManagementFrameworkBundle\\PimcoreCustomerManagementFrameworkBundle','pimcore','1','bool');
 INSERT INTO settings_store (`id`,`scope`,`data`,`type`) VALUES ('BUNDLE_INSTALLED__Web2PrintToolsBundle\\Web2PrintToolsBundle','pimcore','1','bool');
+INSERT INTO settings_store (`id`, `scope`, `data`, `type`)
+VALUES ('BUNDLE_INSTALLED__Pimcore\\Bundle\\DataHubBundle\\PimcoreDataHubBundle', 'pimcore', '1', 'bool');
