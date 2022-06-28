cbe424ad610c3d2b7532797733a85b682414ad7f
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-06-28 11:59:33 +0200

    Revert "Migrate custom_layouts dump to Settings Store (#318)"
    This reverts commit 912e7d32b41534621fb1a602e5cb5e6ccc3cba4a.

---
+++ dump/data-1-custom_layouts.sql
@@ -0,0 +1,5 @@
+
+SET NAMES utf8mb4;
+
+INSERT INTO custom_layouts (`id`,`classId`,`name`,`description`,`creationDate`,`modificationDate`,`userOwner`,`userModification`,`default`) VALUES ('CarReview','CAR','Review','','1566996420','1593598621','2','0','0');
+INSERT INTO custom_layouts (`id`,`classId`,`name`,`description`,`creationDate`,`modificationDate`,`userOwner`,`userModification`,`default`) VALUES ('CarTodo','CAR','Todo','','1566996476','1593598621','2','0','0');
