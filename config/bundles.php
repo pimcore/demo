6aafb0a3ca4d322bed3a0ded90229c83f51c456a
Ashan Ghimire <ashanghimire10@gmail.com>
2022-10-19 10:56:59 +0545

    Added core bundles to bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,13 @@
 <?php
 
+use CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcorePaymentProviderPayPalSmartPaymentButtonBundle;
+
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    PimcoreEcommerceFrameworkBundle::class => ['all' => true],
+    PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
+    PimcorePaymentProviderPayPalSmartPaymentButtonBundle::class => ['all' => true],
 ];
