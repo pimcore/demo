bb3ec36765b536f5fef47a81a7caf714b2c9edbe
Ashan Ghimire <ashanghimire10@gmail.com>
2022-10-19 16:02:24 +0545

    Added missing bundles

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,12 @@
 <?php
 
 use CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle;
+use OutputDataConfigToolkitBundle\OutputDataConfigToolkitBundle;
+use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
 use Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle;
 use Pimcore\Bundle\EcommerceFrameworkBundle\PimcorePaymentProviderPayPalSmartPaymentButtonBundle;
+use Pimcore\Bundle\ObjectMergerBundle\ObjectMergerBundle;
+use Web2PrintToolsBundle\Web2PrintToolsBundle;
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
@@ -10,4 +14,8 @@ return [
     PimcoreEcommerceFrameworkBundle::class => ['all' => true],
     PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
     PimcorePaymentProviderPayPalSmartPaymentButtonBundle::class => ['all' => true],
+    OutputDataConfigToolkitBundle::class => ['all' => true],
+    Web2PrintToolsBundle::class => ['all' => true],
+    PimcoreDataHubBundle::class => ['all' => true],
+    ObjectMergerBundle::class => ['all' => true],
 ];
