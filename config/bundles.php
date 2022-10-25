9348b6d67994ec0387752cb7881230172261728a
Aashan Ghimire <ashanghimire10@gmail.com>
2022-10-25 15:02:24 +0545

    Added core bundles to bundles.php (#368)
    * Added core bundles to bundles.php
    
    * Removed extensions.php as it is no longer supported
    
    * Added missing bundles

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,21 @@
 <?php
 
+use CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle;
+use OutputDataConfigToolkitBundle\OutputDataConfigToolkitBundle;
+use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcorePaymentProviderPayPalSmartPaymentButtonBundle;
+use Pimcore\Bundle\ObjectMergerBundle\ObjectMergerBundle;
+use Web2PrintToolsBundle\Web2PrintToolsBundle;
+
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    PimcoreEcommerceFrameworkBundle::class => ['all' => true],
+    PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
+    PimcorePaymentProviderPayPalSmartPaymentButtonBundle::class => ['all' => true],
+    OutputDataConfigToolkitBundle::class => ['all' => true],
+    Web2PrintToolsBundle::class => ['all' => true],
+    PimcoreDataHubBundle::class => ['all' => true],
+    ObjectMergerBundle::class => ['all' => true],
 ];
