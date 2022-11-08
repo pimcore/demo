b0790a8940343df5aaeba358196560872c7a3b22
Ashan Ghimire <ashanghimire10@gmail.com>
2022-11-08 16:55:39 +0545

    Removed o_ prefixes from more sql files

--- dump/data-1-object_collection_FilterMultiSelect_EF_FD.sql
+++ dump/data-1-object_collection_FilterMultiSelect_EF_FD.sql
@@ -1,4 +1,4 @@
 
 SET NAMES utf8mb4;
 
-INSERT INTO object_collection_FilterMultiSelect_EF_FD (`o_id`,`index`,`fieldname`,`field__tenant`,`field__field`,`field__preSelect`,`scriptPath`,`UseAndCondition`,`label`) VALUES (699,2,'filters',NULL,'carClass','','',0,'carClass');
+INSERT INTO object_collection_FilterMultiSelect_EF_FD (`id`,`index`,`fieldname`,`field__tenant`,`field__field`,`field__preSelect`,`scriptPath`,`UseAndCondition`,`label`) VALUES (699,2,'filters',NULL,'carClass','','',0,'carClass');
--- dump/data-1-object_collection_PricingRule_EF_OSOI.sql
+++ dump/data-1-object_collection_PricingRule_EF_OSOI.sql
@@ -1,5 +1,5 @@
 
 SET NAMES utf8mb4;
 
-INSERT INTO object_collection_PricingRule_EF_OSOI (`o_id`,`index`,`fieldname`,`ruleId`) VALUES (1165,0,'pricingRules',7);
-INSERT INTO object_collection_PricingRule_EF_OSOI (`o_id`,`index`,`fieldname`,`ruleId`) VALUES (1180,0,'pricingRules',7);
+INSERT INTO object_collection_PricingRule_EF_OSOI (`id`,`index`,`fieldname`,`ruleId`) VALUES (1165,0,'pricingRules',7);
+INSERT INTO object_collection_PricingRule_EF_OSOI (`id`,`index`,`fieldname`,`ruleId`) VALUES (1180,0,'pricingRules',7);
