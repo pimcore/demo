2f777ed04fed60dc5650e3c7ff469bae8d20b247
Matthias S <38959016+mattamon@users.noreply.github.com>
2023-01-03 13:28:42 +0100

    [Task] Enable PimcoreGlossaryBundle in config/bundles.php (#385)
    * Enable PimcoreGlossaryBundle in config/bundles.php
    
    * Remove empty line
    
    * Add glossary table to data-0-boostrap.sql
    Add PimcoreGlossaryBundle as installed in data-1-settings_store.sql

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,7 @@ use OutputDataConfigToolkitBundle\OutputDataConfigToolkitBundle;
 use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
 use Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle;
 use Pimcore\Bundle\EcommerceFrameworkBundle\PimcorePaymentProviderPayPalSmartPaymentButtonBundle;
+use Pimcore\Bundle\GlossaryBundle\PimcoreGlossaryBundle;
 use Pimcore\Bundle\ObjectMergerBundle\ObjectMergerBundle;
 use Web2PrintToolsBundle\Web2PrintToolsBundle;
 
@@ -12,9 +13,10 @@ return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     PimcoreEcommerceFrameworkBundle::class => ['all' => true],
     PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
+    PimcoreGlossaryBundle::class => ['all' => true],
     PimcorePaymentProviderPayPalSmartPaymentButtonBundle::class => ['all' => true],
+    PimcoreDataHubBundle::class => ['all' => true],
     OutputDataConfigToolkitBundle::class => ['all' => true],
     Web2PrintToolsBundle::class => ['all' => true],
-    PimcoreDataHubBundle::class => ['all' => true],
     ObjectMergerBundle::class => ['all' => true],
 ];
