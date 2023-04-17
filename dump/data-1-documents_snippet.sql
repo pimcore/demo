9951903c335b8ecb864b2674e9423453463b6f87
Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
2023-04-17 17:45:01 +0200

    Remove columns from dump to make dump work for contentMasterDocumentId and contentMainDocumentId (#470)

--- dump/data-1-documents_snippet.sql
+++ dump/data-1-documents_snippet.sql
@@ -1,16 +1,16 @@
 
 SET NAMES utf8mb4;
 
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (33,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (35,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (36,'App\\Controller\\DefaultController::defaultAction','includes/sidebar.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (106,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (107,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (114,'App\\Controller\\DefaultController::defaultAction','includes/sidebar.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (116,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (117,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (150,'App\\Controller\\DefaultController::defaultAction','includes/sidebar.html.twig',0,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (166,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',33,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (167,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',35,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (168,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',106,NULL);
-INSERT INTO documents_snippet (`id`,`controller`,`template`,`contentMasterDocumentId`,`missingRequiredEditable`) VALUES (169,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',107,NULL);
+INSERT INTO documents_snippet VALUES (33,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (35,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (36,'App\\Controller\\DefaultController::defaultAction','includes/sidebar.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (106,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (107,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (114,'App\\Controller\\DefaultController::defaultAction','includes/sidebar.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (116,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (117,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',NULL,NULL);
+INSERT INTO documents_snippet VALUES (150,'App\\Controller\\DefaultController::defaultAction','includes/sidebar.html.twig',0,NULL);
+INSERT INTO documents_snippet VALUES (166,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',33,NULL);
+INSERT INTO documents_snippet VALUES (167,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',35,NULL);
+INSERT INTO documents_snippet VALUES (168,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',106,NULL);
+INSERT INTO documents_snippet VALUES (169,'App\\Controller\\DefaultController::defaultAction','snippets/standard-teaser.html.twig',107,NULL);
