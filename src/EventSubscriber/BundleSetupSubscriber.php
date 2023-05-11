50f5052da74f150c1d8dda6b02d6b8d7b639ab25
aryaantony92 <arya.antony@pimcore.com>
2023-05-11 12:17:14 +0200

    Add EventSubscriber

---
+++ src/EventSubscriber/BundleSetupSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace App\EventSubscriber;
+
+use Pimcore\Bundle\InstallBundle\Event\BundleSetupEvent;
+use Pimcore\Bundle\InstallBundle\Event\InstallEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class BundleSetupSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            InstallEvents::EVENT_BUNDLE_SETUP => [
+                ['bundleSetup'],
+            ],
+        ];
+    }
+
+    public function bundleSetup(BundleSetupEvent $event): void
+    {
+        // remove bundles to install since they are installed via dump files
+        $event->clearBundlesAndRecommendations();
+    }
+}
