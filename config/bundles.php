e5fac9a31b20c48af1c3bdadfff04b2af05e5cca
Sebastian Blank <blank@data-factory.net>
2023-08-23 12:21:43 +0200

    Do not use deprecated extensions.php (#534)

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,22 @@
 <?php
 
+use CustomerManagementFrameworkBundle\PimcoreCustomerManagementFrameworkBundle;
+use OutputDataConfigToolkitBundle\OutputDataConfigToolkitBundle;
+use Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcorePaymentProviderPayPalSmartPaymentButtonBundle;
+use Pimcore\Bundle\ObjectMergerBundle\ObjectMergerBundle;
+use Presta\SitemapBundle\PrestaSitemapBundle;
+use Web2PrintToolsBundle\Web2PrintToolsBundle;
+
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
+    PimcoreEcommerceFrameworkBundle::class => ['all' => true],
+    PimcoreCustomerManagementFrameworkBundle::class => ['all' => true],
+    PimcorePaymentProviderPayPalSmartPaymentButtonBundle::class => ['all' => true],
+    PimcoreDataHubBundle::class => ['all' => true],
+    OutputDataConfigToolkitBundle::class => ['all' => true],
+    Web2PrintToolsBundle::class => ['all' => true],
+    ObjectMergerBundle::class => ['all' => true],
+    PrestaSitemapBundle::class => ['all' => true],
 ];
