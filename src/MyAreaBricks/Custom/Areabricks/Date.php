7a764714976a2a52a780e922eddf7bc3b1895f9d
aryaantony92 <arya.antony@pimcore.com>
2023-10-24 17:13:18 +0200

    Add example for Areabrick

--- src/MyAreaBricks/Custom/Areabricks/Date.php
+++ src/MyAreaBricks/Custom/Areabricks/Date.php
@@ -5,16 +5,11 @@ namespace App\MyAreaBricks\Custom\Areabricks;
 use Pimcore\Extension\Document\Areabrick\Attribute\AsAreabrick;
 use Pimcore\Extension\Document\Areabrick\AbstractTemplateAreabrick;
 
-#[AsAreabrick(id: 'iframe')]
-class Iframe extends AbstractTemplateAreabrick
+#[AsAreabrick(id: 'date')]
+class Date extends AbstractTemplateAreabrick
 {
     public function getName(): string
     {
-        return 'Iframe';
-    }
-
-    public function getDescription(): string
-    {
-        return 'Embed contents from other URL (websites) via iframe';
+        return 'Date';
     }
 }
