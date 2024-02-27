d0599e2443c3695e0597227b6d9ec0e9acd2ce8c
JiaJia Ji <kingjia90@gmail.com>
2024-02-27 12:40:04 +0100

    [Task]: Make booting consistent (#570)

--- public/index.php
+++ public/index.php
@@ -20,14 +20,14 @@ use Symfony\Component\HttpFoundation\Request;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 Bootstrap::setProjectRoot();
-Bootstrap::bootstrap();
 
 return function (Request $request, array $context) {
 
     // set current request as property on tool as there's no
     // request stack available yet
     Tool::setCurrentRequest($request);
-
+    
+    Bootstrap::bootstrap();
     $kernel = Bootstrap::kernel();
 
     // reset current request - will be read from request stack from now on
